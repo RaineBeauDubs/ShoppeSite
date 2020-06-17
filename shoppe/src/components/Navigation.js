e9af059d03755e6873d4168f2cc67293cb6b9973
Raine Wallace <rainebeaudubs@gmail.com>
2020-06-17 13:56:43 -0400

    created nav comp, added links, wired to app

---
+++ shoppe/src/components/Navigation.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navigation = () => {
+  return (
+    <div className="navBar">
+      <h1>Rainy Day Resale</h1>
+      <div className="nav">
+        <NavLink className="navLink" to="/">
+          <h3>Ladies</h3>
+        </NavLink>
+        <NavLink className="navLink" to="/">
+          <h3>Juniors</h3>
+        </NavLink>
+        <NavLink className="navLink" to="/">
+          <h3>Accessories</h3>
+        </NavLink>
+        <NavLink className="navLink" to="/">
+          <h3>Consignors</h3>
+        </NavLink>
+        <NavLink className="navLink" to="/">
+          <h3>Contact Us</h3>
+        </NavLink>
+      </div>
+    </div>
+  )
+}
+
+export default Navigation;
