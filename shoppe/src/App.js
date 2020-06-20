6a29faa6ec6bbdc41958357445ade22c4dccb78f
Raine Wallace <rainebeaudubs@gmail.com>
2020-06-19 21:53:05 -0400

    added all routes

--- shoppe/src/App.js
+++ shoppe/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Navigation from './components/navigation/Navigation';
 import Home from './components/home/Home';
+import Ladies from './components/ladies/Ladies';
+import Juniors from './components/juniors/Juniors';
+import Accessories from './components/accessories/Accessories';
+import Studio from './components/studio/Studio';
+import Consignors from './components/consignors/Consignors';
+import Community from './components/community/Community';
+import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
 import { Route } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-  faFacebookSquare
-} from '@fortawesome/free-brands-svg-icons'
-import {
-  faUmbrella
-} from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 
 function App() {
@@ -23,6 +23,34 @@ function App() {
         path="/"
         component={Home}
         exact
+      />
+      <Route 
+        path="/ladies"
+        component={Ladies}
+      />
+      <Route 
+        path="/juniors"
+        component={Juniors}
+      />
+      <Route 
+        path="/accessories"
+        component={Accessories}
+      />
+      <Route 
+        path="/studio"
+        component={Studio}
+      />
+      <Route 
+        path="/consignors"
+        component={Consignors}
+      />
+      <Route 
+        path="/community"
+        component={Community}
+      />
+      <Route 
+        path="/contact"
+        component={Contact}
       />
       <Footer />
     </div>
