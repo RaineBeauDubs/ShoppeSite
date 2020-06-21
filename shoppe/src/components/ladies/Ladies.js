0eebfe732b3fb070fb4165b7d07dad916b72ccf0
Raine Wallace <rainebeaudubs@gmail.com>
2020-06-20 23:44:13 -0400

    added ladies text

--- shoppe/src/components/ladies/Ladies.js
+++ shoppe/src/components/ladies/Ladies.js
@@ -4,7 +4,8 @@ import './ladies.css';
 function Ladies() {
   return (
     <div className="ladiesContainer">
-      <p>I will be the Ladies page! :D</p>
+      <h2 className="colorHeader">Ladies</h2>
+      <p>Our ladies sizes run from x-small to 3x and sometimes larger, depending on what we get in. We carry popular ladies brands including Chicos, Land's End, White House Black Market, Coldwater Creek, Lane Bryant, Talbots, and many more!</p>
     </div>
   );
 }
