dbf60a7901bbb7f1015ae5f4340c132939489030
Raine Wallace <rainebeaudubs@gmail.com>
2020-06-21 03:57:10 -0400

    added images to ladies page, organized css sheet for styling

--- shoppe/src/components/ladies/Ladies.js
+++ shoppe/src/components/ladies/Ladies.js
@@ -7,12 +7,45 @@ function Ladies() {
       <h2 className="colorHeader">Ladies</h2>
       <div className="ladiesFlexContainer">
         <div className="ladiesFlexLeft">
-        
+          <img 
+            className="logoLeft brandLogo" 
+            src="/images/LLogoChicos.png" 
+            alt="Chicos" 
+          />
+          <img 
+            className="logoRight brandLogo" 
+            src="/images/LLogoColdCreek.png" 
+            alt="ColdwaterCreek" 
+          />
+          <img 
+            className="logoLeft brandLogo" 
+            src="/images/LLogoLane.png" 
+            alt="Lane Bryant" 
+          />
+          <img 
+            className="logoRight brandLogo" 
+            src="/images/LLogoLilly.png" 
+            alt="Lilly Pulitzer" 
+          />
+          <img 
+            className="logoLeft brandLogo" 
+            src="/images/LLogoTalbots.png" 
+            alt="Talbots" 
+          />
+          <img 
+            className="logoRight brandLogo" 
+            src="/images/LLogoTorrid.png" 
+            alt="Torrid"
+          />
+          <img 
+            className="logoLeft brandLogo" 
+            src="/images/LLogoWHBM.png" 
+            alt="White House | Black Market" 
+          />
         </div>
         <div className="ladiesFlexRight">
           <p>Our ladies sizes run from x-small to 3x and sometimes larger, depending on what we get in. We carry popular ladies brands including Chicos, Land's End, White House Black Market, Coldwater Creek, Lane Bryant, Talbots, and many more!</p>
         </div>
-
       </div>
     </div>
   );
