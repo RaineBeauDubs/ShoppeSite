e7714afea73288033f9649ed25eabc9746d64a15
Raine Wallace <rainebeaudubs@gmail.com>
2020-06-21 03:44:53 -0400

    added logo imgs, created flex unstyled flex containers

--- shoppe/src/components/ladies/Ladies.js
+++ shoppe/src/components/ladies/Ladies.js
@@ -5,7 +5,15 @@ function Ladies() {
   return (
     <div className="ladiesContainer">
       <h2 className="colorHeader">Ladies</h2>
-      <p>Our ladies sizes run from x-small to 3x and sometimes larger, depending on what we get in. We carry popular ladies brands including Chicos, Land's End, White House Black Market, Coldwater Creek, Lane Bryant, Talbots, and many more!</p>
+      <div className="ladiesFlexContainer">
+        <div className="ladiesFlexLeft">
+        
+        </div>
+        <div className="ladiesFlexRight">
+          <p>Our ladies sizes run from x-small to 3x and sometimes larger, depending on what we get in. We carry popular ladies brands including Chicos, Land's End, White House Black Market, Coldwater Creek, Lane Bryant, Talbots, and many more!</p>
+        </div>
+
+      </div>
     </div>
   );
 }
