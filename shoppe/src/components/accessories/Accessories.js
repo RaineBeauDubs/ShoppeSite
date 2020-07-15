74041e21e9a8b9ffc70ef6c5225b79571e8ee1c6
Raine Wallace <rainebeaudubs@gmail.com>
2020-07-15 17:40:17 -0400

    added and styled jewelry imgs

--- shoppe/src/components/accessories/Accessories.js
+++ shoppe/src/components/accessories/Accessories.js
@@ -5,6 +5,23 @@ function Accessories() {
   return (
     <div className="accessoriesContainer">
       <h2 className="colorHeader">Accessories</h2>
+      <div className="jewelryImgCont">
+      <img
+          className="jwlPic"
+          src="/images/AccPic1.jpg"
+          alt="A sample of our jewelry"
+        />
+        <img
+          className="jwlPic"
+          src="/images/AccPic2.jpg"
+          alt="A sample of our jewelry"
+        />
+        <img
+          className="jwlPic"
+          src="/images/AccPic3.jpg"
+          alt="A sample of our jewelry"
+        />
+      </div>
       <p>Shoes - ladies sizes 5 to 12</p>
       <p>Jewelry - consigned, locally-made, vintage, etc.</p>
       <p>Purses - both fashion and popular brands such as Coach, and Vera Bradley</p>
