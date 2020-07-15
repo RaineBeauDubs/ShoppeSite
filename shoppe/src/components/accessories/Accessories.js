71a74d702b00c7984e7a7095634906f25549510a
Raine Wallace <rainebeaudubs@gmail.com>
2020-07-15 18:43:44 -0400

    updated and styled acc descs

--- shoppe/src/components/accessories/Accessories.js
+++ shoppe/src/components/accessories/Accessories.js
@@ -6,7 +6,7 @@ function Accessories() {
     <div className="accessoriesContainer">
       <h2 className="colorHeader">Accessories</h2>
       <div className="jewelryImgCont">
-      <img
+        <img
           className="jwlPic"
           src="/images/AccPic1.jpg"
           alt="A sample of our jewelry"
@@ -22,10 +22,22 @@ function Accessories() {
           alt="A sample of our jewelry"
         />
       </div>
-      <p>Shoes - ladies sizes 5 to 12</p>
-      <p>Jewelry - consigned, locally-made, vintage, etc.</p>
-      <p>Purses - both fashion and popular brands such as Coach, and Vera Bradley</p>
-      <p>Galleria Umbrellas - twenty to thirty styles including those pictured below</p>
+      <div className="accDesc">
+        <h3 className="accTitle">Jewelry</h3>
+        <p>- consigned, locally-made, vintage, we've got it all!</p>
+      </div>
+      <div className="accDesc">
+        <h3 className="accTitle">Shoes</h3>
+        <p>- ladies sizes 5 to 12 in all the brands you love!</p>
+      </div>
+      <div className="accDesc">
+        <h3 className="accTitle">Purses</h3>
+        <p>- both fashion and popular brands such as Coach, and Vera Bradley</p>
+      </div>
+      <div className="accDesc">
+        <h3 className="accTitle">Galleria Umbrellas</h3>
+        <p>- gorgeous, sturdy, collapsable (or inverted!) umbrellas from Galleria</p>
+      </div>
     </div>
   );
 }
