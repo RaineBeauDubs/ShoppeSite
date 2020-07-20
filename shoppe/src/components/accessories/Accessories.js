b691b2741d856a0bee2565e047c19641f9cbdae8
Raine Wallace <rainebeaudubs@gmail.com>
2020-07-20 16:15:58 -0400

    added shoe img, veeery loose styling, game plan

--- shoppe/src/components/accessories/Accessories.js
+++ shoppe/src/components/accessories/Accessories.js
@@ -26,13 +26,20 @@ function Accessories() {
         <h3 className="accTitle">Jewelry</h3>
         <p>- consigned, locally-made, vintage, we've got it all!</p>
       </div>
+      <div className="jewelryImgCont">
       <div className="accDesc">
         <h3 className="accTitle">Shoes</h3>
         <p>- ladies sizes 5 to 12 in all the brands you love!</p>
       </div>
+      <img
+          className="shoePic"
+          src="/images/AccShoes.png"
+          alt="Just a small sample of our huge shoe selection!"
+        />
       <div className="accDesc">
         <h3 className="accTitle">Purses</h3>
         <p>- both fashion and popular brands such as Coach, and Vera Bradley</p>
+      </div>
       </div>
       <div className="accDesc">
         <h3 className="accTitle">Galleria Umbrellas</h3>
