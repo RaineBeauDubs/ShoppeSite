a6594473d671b645917c9437b40e365c507be887
Raine Wallace <rainebeaudubs@gmail.com>
2020-07-27 13:33:54 -0400

    start on contact page

--- shoppe/src/components/contact/Contact.js
+++ shoppe/src/components/contact/Contact.js
@@ -5,6 +5,9 @@ function Contact() {
   return (
     <div className="contactContainer">
       <h2 className="colorHeader">Get In Touch!</h2>
+      <h3 className="colorSubHeader">Where are we?</h3>
+      <h3 className="colorSubHeader">Questions?</h3>
+      <h3 className="colorSubHeader">Send us an email:</h3>
     </div>
   );
 }
