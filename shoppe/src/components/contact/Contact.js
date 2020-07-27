9caab51cc69f3fd21092daa8ff605fc4f89bbb4a
Raine Wallace <rainebeaudubs@gmail.com>
2020-07-27 17:46:04 -0400

    contact moderately styled, still have much to do, but want to get redirects workingg

--- shoppe/src/components/contact/Contact.js
+++ shoppe/src/components/contact/Contact.js
@@ -5,18 +5,24 @@ function Contact() {
   return (
     <div className="contactContainer">
       <h2 className="colorHeader">Get In Touch!</h2>
-      <h3 className="colorSubHeader">Come find us at:</h3>
-      <p>102 East Main Street</p>
-      <p>LaGrange, KY 40031</p>
-      <img
-        className="fakeMap"
-        src="/images/ConPlaceholderMap.png"
-        alt="Where we're located in LaGrange"
-      />
-      <h3 className="colorSubHeader">Questions? Call us at:</h3>
-      <p>(502) 222-2225</p>
-      <h3 className="colorSubHeader">Send us an email:</h3>
-      <p>RainyDayResale@gmail.com</p>
+      <div className="contactSec">
+        <h3 className="colorSubHeader">Come find us at:</h3>
+        <p>102 East Main Street</p>
+        <p>LaGrange, KY 40031</p>
+        <img
+          className="fakeMap"
+          src="/images/ConPlaceholderMap.png"
+          alt="Where we're located in LaGrange"
+        />
+      </div>
+      <div className="contactSec">
+        <h3 className="colorSubHeader">Questions? Call us at:</h3>
+        <p>(502) 222-2225</p>
+      </div>
+      <div className="contactSec">
+        <h3 className="colorSubHeader">Send us an email:</h3>
+        <p>RainyDayResale@gmail.com</p>
+      </div>
     </div>
   );
 }
