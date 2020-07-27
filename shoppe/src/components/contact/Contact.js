aa8aad13ccf2e765039f0da1a742d85467143337
Raine Wallace <rainebeaudubs@gmail.com>
2020-07-27 17:43:01 -0400

    added map image as placeholder, added contact info

--- shoppe/src/components/contact/Contact.js
+++ shoppe/src/components/contact/Contact.js
@@ -5,9 +5,18 @@ function Contact() {
   return (
     <div className="contactContainer">
       <h2 className="colorHeader">Get In Touch!</h2>
-      <h3 className="colorSubHeader">Where are we?</h3>
-      <h3 className="colorSubHeader">Questions?</h3>
+      <h3 className="colorSubHeader">Come find us at:</h3>
+      <p>102 East Main Street</p>
+      <p>LaGrange, KY 40031</p>
+      <img
+        className="fakeMap"
+        src="/images/ConPlaceholderMap.png"
+        alt="Where we're located in LaGrange"
+      />
+      <h3 className="colorSubHeader">Questions? Call us at:</h3>
+      <p>(502) 222-2225</p>
       <h3 className="colorSubHeader">Send us an email:</h3>
+      <p>RainyDayResale@gmail.com</p>
     </div>
   );
 }
