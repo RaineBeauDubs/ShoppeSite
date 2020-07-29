1c5e28861000eb07c4ca86ddc49d6c01e0bc1b0c
Raine Wallace <rainebeaudubs@gmail.com>
2020-07-29 16:17:16 -0400

    added top flex info, structure

--- shoppe/src/components/community/Community.js
+++ shoppe/src/components/community/Community.js
@@ -5,6 +5,25 @@ function Community() {
   return (
     <div className="communityContainer">
       <h2 className="colorHeader">Community</h2>
+      <h3 className="colorSubHeader">Make sure to check out all these other awesome places in our town!</h3>
+      <div className="comTopFlexCont">
+        <div className="comTopFlexSec">
+          <img
+            className="comTopFlexPic"
+            src="/images/ComDowntownBarber.jpg"
+            alt="Downtown Barber is open for business!"
+          />
+          <p>Downtown Barber is located at 107 N. 1st St. and can be accessed from Main Street through Rainy Day Resale. Fantastic, experienced barbers can be found here, performing their hair magic on all!</p>
+        </div>
+        <div className="comTopFlexSec">
+          <p>La Grange Farmers' Market & Artisans is a Kentucky Proud market and a La Grange Kentucky Main Street Event. Open on the courthouse lawn (100 W. Main St.) from 9AM to 1PM on Saturdays during Farmers' Market season!</p>
+          <img
+            className="comTopFlexPic"
+            src="/images/ComLGFMPoster.png"
+            alt="La Grange Farmers' Market Poster by Stephen Winter"
+          />
+        </div>
+      </div>
       {/* Link to LG Main Street page
       https://www.lagrangemainstreet.org/
       add images
