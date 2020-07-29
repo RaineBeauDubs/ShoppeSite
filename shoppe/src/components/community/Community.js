3ca20f44e020c98e26507e93f76c0f0eefd80d6e
Raine Wallace <rainebeaudubs@gmail.com>
2020-07-29 14:00:01 -0400

    added notes, added images

--- shoppe/src/components/community/Community.js
+++ shoppe/src/components/community/Community.js
@@ -6,8 +6,14 @@ function Community() {
     <div className="communityContainer">
       <h2 className="colorHeader">Community</h2>
       {/* Link to LG Main Street page
+      https://www.lagrangemainstreet.org/
+      add images
       link to barber shop social media (make quick bbs site?)
+      https://www.facebook.com/DowntownBarberKY
       link to main street social media 
+      https://www.facebook.com/LaGrangeKYMainStreetProgram
+      https://www.facebook.com/lagrangefarmersmkt
+      https://www.facebook.com/places/Things-to-do-in-La-Grange-Kentucky/109376089080334
       Maybe yearly events? */}
     </div>
   );
