37884ad24b1c95dcc292c145ef07d87faed3ccf9
Raine Wallace <rainebeaudubs@gmail.com>
2020-07-29 16:45:48 -0400

    styled top sec

--- shoppe/src/components/community/Community.js
+++ shoppe/src/components/community/Community.js
@@ -13,10 +13,22 @@ function Community() {
             src="/images/ComDowntownBarber.jpg"
             alt="Downtown Barber is open for business!"
           />
-          <p>Downtown Barber is located at 107 N. 1st St. and can be accessed from Main Street through Rainy Day Resale. Fantastic, experienced barbers can be found here, performing their hair magic on all!</p>
+          <p className="comTopFlexDesc">Downtown Barber is located at 107 North First Street and can also be accessed from Main Street through Rainy Day Resale.<br />Fantastic, experienced barbers can be found here, performing their hair magic on all!<br /><br />
+            <a 
+              href="https://www.facebook.com/DowntownBarberKY" 
+              target="_blank" 
+              className="comFBLink">
+                Check them out on Facebook here!
+            </a></p>
         </div>
         <div className="comTopFlexSec">
-          <p>La Grange Farmers' Market & Artisans is a Kentucky Proud market and a La Grange Kentucky Main Street Event. Open on the courthouse lawn (100 W. Main St.) from 9AM to 1PM on Saturdays during Farmers' Market season!</p>
+          <p className="comTopFlexDesc">La Grange Farmers' Market & Artisans is a Kentucky Proud market and a La Grange Kentucky Main Street Event.<br />Open on the courthouse lawn (100 West Main Street) from 9AM to 1PM on Saturdays during Farmers' Market season!<br /><br />
+            <a 
+              href="https://www.facebook.com/lagrangefarmersmkt" 
+              target="_blank" 
+              className="comFBLink">
+                Check them out on Facebook here!
+            </a></p>
           <img
             className="comTopFlexPic"
             src="/images/ComLGFMPoster.png"
@@ -28,7 +40,7 @@ function Community() {
       https://www.lagrangemainstreet.org/
       add images
       link to barber shop social media (make quick bbs site?)
-      https://www.facebook.com/DowntownBarberKY
+      
       link to main street social media 
       https://www.facebook.com/LaGrangeKYMainStreetProgram
       https://www.facebook.com/lagrangefarmersmkt
