c31efa8fef4c018b2cf278e443393b471e3cd4ca
Raine Wallace <rainebeaudubs@gmail.com>
2020-07-29 17:02:54 -0400

    styled bottom info, looks much better than top half, gotta figure out how to pretty that up

--- shoppe/src/components/community/Community.js
+++ shoppe/src/components/community/Community.js
@@ -38,7 +38,7 @@ function Community() {
       </div>
       <div className="comBottomCont">
         <img
-          className="comBottomPicHeader"
+          className="comBottomPic"
           src="/images/ComLGMSHeader.png"
           alt="La Grange Street information"
         />
@@ -72,16 +72,6 @@ function Community() {
             alt="La Grange Street information"
           />
       </div>
-      {/* Link to LG Main Street page
-      https://www.lagrangemainstreet.org/
-      add images
-      link to barber shop social media (make quick bbs site?)
-      
-      link to main street social media 
-      https://www.facebook.com/LaGrangeKYMainStreetProgram
-      https://www.facebook.com/lagrangefarmersmkt
-      https://www.facebook.com/places/Things-to-do-in-La-Grange-Kentucky/109376089080334
-      Maybe yearly events? */}
     </div>
   );
 }
