afb97f6b52a9808b29d7c2cb5341b78051b647e7
Raine Wallace <rainebeaudubs@gmail.com>
2020-08-03 18:18:58 -0400

    added notes for changes at client's request

--- shoppe/src/components/community/Community.js
+++ shoppe/src/components/community/Community.js
@@ -80,4 +80,9 @@ function Community() {
   );
 }
 
+// Add link to shop site com, add ghost tour info
+// move utopia studio to community, smaller thing, one pic
+// switch com w/contact us in nav bar
+// 
+
 export default Community;
